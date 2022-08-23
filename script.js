f63644f254ba04abe3668ebc729173d04c96f244
henriquesvsv <henriquedasilva467@gmail.com>
2022-08-22 22:32:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,36 @@ document.addEventListener('DOMContentLoaded' , () => {
 const personagem  = document.querySelector('.personagem');
 var personagemleft = 0;
 
-function andar(){
-    personagemleft+=20;
-    personagem.style.left=personagemleft+'px';
-    console.log(personagemleft);
+function andar(direcao){
+    switch(direcao){
+        case 'right':
+            personagemleft-=20;
+            personagem.style.left=personagemleft+'px';
+            console.log(personagemleft);
+            break;
+        case 'left':
+            personagemleft+=20;
+            personagem.style.left=personagemleft+'px';
+            console.log(personagemleft);
+            break;
+       
+       
+    }
+    
 }
 
 document.addEventListener('keyup', control)
 document.addEventListener('keypress', control)
 
 function control(e) {
-    if (e.keyCode === 32) {
-        andar()
+    switch(e.keyCode){
+    case 68:
+        andar('left');
+        break;
+    
+    case 65:
+        andar('right');
+        break;
     }
 }
 
